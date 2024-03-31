0f91bbb8ed6660a752f29b0ed4a8fabe41379fc1
daniskzn <danis.sharafiev@bk.ru>
2024-04-01 00:21:31 +0300

    activate_db_grant feature

---
+++ src/adapters/database/postgresql.py
@@ -0,0 +1,3 @@
+from adapters.database.base import DatabaseAdapter
+from core.models import User
+
---
+++ src/core/forms/admin_grant.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class ActivateGrantForm(forms.Form):
+    _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
+    command = forms.CharField(
+        label="Найдите шаблон команды в таблице скриптов и измените его здесь",
+        required=True,
+        widget=forms.Textarea,
+    )
